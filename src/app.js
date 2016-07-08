3bf900659ecfd5d4d9fa0c90f65f8192d758a748
cbasler <clemens.basler@1und1.de>
2016-07-08 12:51:07 +0200

    adding angular in the app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
- import cats from './cats';
- import $ from 'jquery';
+import cats from './cats';
+import $ from 'jquery';
+import angular from 'angular';
 
  $('<h1>Cats</h1>').appendTo('body');
  const ul = $('<ul></ul>').appendTo('body');
